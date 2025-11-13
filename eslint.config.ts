d34dcdcc1ffbbc54b2cd69915545ff3c7b8bd141
lxf <infiniment@163.com>
2025-11-13 15:54:39 +0800

    release: v1.2.0

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,10 @@
+import { defineConfig } from 'eslint/config'
 
-export default [
+export default defineConfig([
   {
     ignores: ["dist/*", "node_modules/*", "coverage/*", 'coverage'],
     rules: {
       "unicorn/no-array-callback-reference": 0
     }
   }
-]
+])
